846efb2d5d0af2326d9b7e07ec2726f41c145c98
JPham4801 <JPham4801@gmail.com>
2025-04-03 18:32:24 -0500

    implemented working CRUD

--- controllers/binder-controller.js
+++ controllers/binder-controller.js
@@ -3,6 +3,7 @@ const verifyToken = require('../middleware/verify-token.js');
 const Binder = require('../models/Binder.js');
 const router = express.Router();
 
+// CREATE Binder
 router.post('/', verifyToken, async (req, res) => {
   try {
     req.body.author = req.user._id;
@@ -15,8 +16,54 @@ router.post('/', verifyToken, async (req, res) => {
   }
 });
 
-router.get("/", verifyToken, async (req, res) => {
-  
-})
+// READ (Show) Binder
+router.get('/:binderId', verifyToken, async (req, res) => {
+  try {
+    const binder = await Binder.findById(req.params.binderId).populate('author');
+    res.status(200).json(binder);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// UPDATE Binder
+router.put('/:binderId', verifyToken, async (req, res) => {
+  try {
+    // find binder
+    const binder = await Binder.findById(req.params.binderId);
+
+    // check permissions
+    if (!binder.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+
+    // update binder
+    const updatedBinder = await Binder.findByIdAndUpdate(req.params.binderId, req.body, { new: true });
+
+    // append req.user to the author property
+    updatedBinder._doc.author = req.user;
+
+    // issue JSON response
+    res.status(200).json(updatedBinder);
+  } catch {
+    res.status(err).json({ err: err.message });
+  }
+});
+
+// delete Binder
+router.delete('/:binderId', verifyToken, async (req, res) => {
+  try {
+    const binder = await Binder.findById(req.params.binderId);
+
+    if (!binder.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+
+    const deleteBinder = await Binder.findByIdAndDelete(req.params.binderId);
+    res.status(200).json(deleteBinder);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
 
 module.exports = router;
--- models/Binder.js
+++ models/Binder.js
@@ -5,22 +5,107 @@ const cardSchema = new mongoose.Schema(
   {
     apiId: { type: String, required: true }, // From Pokémon TCG API
     name: { type: String, required: true },
-    images: {
-      small: String,
-      large: String,
-    },
+    level: String,
+    hp: String,
+    supertype: String,
+    subtypes: [String],
+    types: [String],
+    evolvesFrom: String,
+    abilities: [
+      {
+        name: String,
+        text: String,
+        type: String,
+      },
+    ],
+    attacks: [
+      {
+        name: String,
+        cost: [String],
+        convertedEnergyCost: Number,
+        damage: String,
+        text: String,
+      },
+    ],
+    weaknesses: [
+      {
+        type: String,
+        value: String,
+      },
+    ],
+    resistances: [
+      {
+        type: String,
+        value: String,
+      },
+    ],
+    retreatCost: [String],
+    convertedRetreatCost: Number,
     set: {
       id: String,
       name: String,
       series: String,
+      printedTotal: Number,
+      total: Number,
+      legalities: {
+        unlimited: String,
+      },
+      ptcgoCode: String,
+      releaseDate: String,
+      updatedAt: String,
+      images: {
+        symbol: String,
+        logo: String,
+      },
     },
-    supertype: String,
-    subtypes: [String],
+    number: String,
+    artist: String,
     rarity: String,
-    hp: String,
-    types: [String],
+    flavorText: String,
+    nationalPokedexNumbers: [Number],
+    legalities: {
+      unlimited: String,
+    },
+    images: {
+      small: String,
+      large: String,
+    },
+    tcgplayer: {
+      url: String,
+      updatedAt: String,
+      prices: {
+        holofoil: {
+          low: Number,
+          mid: Number,
+          high: Number,
+          market: Number,
+          directLow: Number,
+        },
+      },
+    },
+    cardmarket: {
+      url: String,
+      updatedAt: String,
+      prices: {
+        averageSellPrice: Number,
+        lowPrice: Number,
+        trendPrice: Number,
+        germanProLow: Number,
+        suggestedPrice: Number,
+        reverseHoloSell: Number,
+        reverseHoloLow: Number,
+        reverseHoloTrend: Number,
+        lowPriceExPlus: Number,
+        avg1: Number,
+        avg7: Number,
+        avg30: Number,
+        reverseHoloAvg1: Number,
+        reverseHoloAvg7: Number,
+        reverseHoloAvg30: Number,
+      },
+    },
   },
-  { _id: false } // Don't generate a Mongo ID for embedded cards
+  { _id: false }
 );
 
 const binderSchema = new mongoose.Schema(
@@ -60,7 +145,7 @@ const binderSchema = new mongoose.Schema(
     },
     masterSetCount: {
       type: Number,
-      required: true,
+      default: 1,
     },
     author: {
       type: mongoose.Schema.Types.ObjectId,
